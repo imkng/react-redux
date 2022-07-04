27eaf32dad81412f9f9b172c0516fde0b274eafc
Krishna <1india.kng@gmail.com>
2022-07-04 17:43:29 +0530

    counter App

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import {createStore} from 'redux';
+
+const reducerFn = (state = {counter : 0},action) =>{
+
+    if(action.type === "INC"){
+        return {counter : state.counter + 1};
+    }
+    if(action.type === "DEC"){
+        if(state.counter <= 0){
+            return {counter: 0};
+        }
+        return {counter : state.counter - 1};
+    }
+    if(action.type === "ADDBY"){
+        return {counter: state.counter + action.payload};
+    }
+    return state;
+}
+
+const store = createStore(reducerFn);
+export default store;
